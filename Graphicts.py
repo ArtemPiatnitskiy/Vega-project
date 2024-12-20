2df21a6a81e5ff7bd9c00514ebdd9e0eadf6e423
ArtemPiatnitskiy <ARTnov78095@outlook.com>
2024-12-20 21:11:04 +0300

    Графики и сбор данных

--- Graphicts.py
+++ Graphicts.py
@@ -1,135 +1,84 @@
+import matplotlib
 import numpy as np
+
+matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Имя файла
-# filename = 'result.txt'
-filename = 'Этап 1.txt'
+times = list()
+height = list()
+velocity_x = list()
+velocity_y = list()
+velocity_z = list()
+speed = list()
+position_x = list()
+position_y = list()
+position_z = list()
 
-# Инициализация списков для данных
-time_data = []
-altitude_data = []
-velocity_x_data = []
-velocity_y_data = []
-velocity_z_data = []
-speed_data = []
-position_x_data = []
-position_y_data = []
-position_z_data = []
+pos_xmy = list()
 
-# Чтение файла и загрузка данных
-with open(filename, 'r') as file:
-    for line in file:
-        # Очистка строки от возможных проблемных символов
-        cleaned_line = line.strip().replace(',', '')  # Убираем запятые и пробелы
-        values = cleaned_line.split()
-        try:
-            time_data.append(float(values[0]))          # Время
-            altitude_data.append(float(values[1]))      # Высота
-            velocity_x_data.append(float(values[2]))    # Скорость по X
-            velocity_y_data.append(float(values[3]))    # Скорость по Y
-            velocity_z_data.append(float(values[4]))    # Скорость по Z
-            speed_data.append(float(values[5]))         # Модуль скорости
-            position_x_data.append(float(values[6]))    # Позиция X
-            position_y_data.append(float(values[7]))    # Позиция Y
-            position_z_data.append(float(values[8]))    # Позиция Z
-        except (IndexError, ValueError) as e:
-            print(f"Ошибка в строке: {line}")
-            print(f"Сообщение: {e}")
+with open("result.txt") as file:
+    for i in file.readlines():
+        t, h, vx, vy, vz, sp, px, py, pz = i.split(" ")
+        times.append(float(t))
+        height.append(float(h))
+        velocity_x.append(float(vx))
+        velocity_y.append(float(vy))
+        velocity_z.append(float(vz))
+        speed.append(float(sp))
+        position_x.append(float(px))
+        position_y.append(float(py))
+        position_z.append(float(pz))
+        pos_xmy.append(abs(float(px) - float(py)))
 
-# Преобразуем списки в массивы numpy
-time_data = np.array(time_data)
-altitude_data = np.array(altitude_data)
-velocity_x_data = np.array(velocity_x_data)
-velocity_y_data = np.array(velocity_y_data)
-velocity_z_data = np.array(velocity_z_data)
-speed_data = np.array(speed_data)
-position_x_data = np.array(position_x_data)
-position_y_data = np.array(position_y_data)
-position_z_data = np.array(position_z_data)
+def F(n1, n2, n3, lst_x, lst_y, t_x, t_y):
+    plt.subplot(n1, n2, n3)
+    plt.plot(lst_x, lst_y)
+    plt.xlabel(t_x)
+    plt.ylabel(t_y)
+    plt.grid()
 
-# Построение графиков
-plt.figure(figsize=(12, 12))
+def speed_graf():
+    F(2, 2, 1, times, velocity_x, "Время (с)", "Скорость по X (м/c)")
+    F(2, 2, 2, times, velocity_y, "Время (с)", "Скорость по Y (м/c)")
+    F(2, 2, 3, times, velocity_z, "Время (с)", "Скорость по Z (м/c)")
+    F(2, 2, 4, times, speed, "Время (с)", "Скорость общая (м/c)")
 
-# 1. Скорости от времени
-plt.subplot(3, 2, 1)
-plt.plot(time_data, velocity_x_data, label='Vx')
-plt.plot(time_data, velocity_y_data, label='Vy')
-plt.plot(time_data, velocity_z_data, label='Vz')
-plt.xlabel('Время (s)')
-plt.ylabel('Скорость (м/с)')
-plt.title('Скорости по осям от времени')
-plt.legend()
-plt.grid()
+def coords_graf():
+    F(2, 2, 1, times, position_x, "Время (с)", "Координаты по X")
+    F(2, 2, 2, times, position_y, "Время (с)", "Координаты по Y")
+    F(2, 2, 3, times, position_z, "Время (с)", "Координаты по Z")
 
-# 2. Модуль скорости от времени
-plt.subplot(3, 2, 2)
-plt.plot(time_data, speed_data, label='|V|', color='red')
-plt.xlabel('Время (s)')
-plt.ylabel('Скорость (м/с)')
-plt.title('Модуль скорости от времени')
-plt.grid()
+def traectory_graf():
+    F(2, 2, 1, position_x, position_y, "Координата по X", "Координата по Y")
+    F(2, 2, 2, position_y, position_z, "Координата по Y", "Координата по Z")
+    F(2, 2, 3, position_z, position_x, "Координата по Z", "Координата по X")
 
-# 3. Модуль скорости от высоты
-plt.subplot(3, 2, 3)
-plt.plot(altitude_data, time_data, label='t', color='green')
-plt.xlabel('Высота (м)')
-plt.ylabel('Время (м/с)')
-plt.title('Высота от времени')
-plt.grid()
+def height_graf():
+    F(2, 2, 1, height, times, "Высота (м)", "Время (с)")
+    F(2, 2, 2, height, speed, "Высота (м)", "Скорость (м/c)")
+    plt.gca().invert_xaxis()
 
-# 4. Траектория (график по скорости)
-plt.subplot(3, 2, 4)
-plt.plot(velocity_x_data, velocity_y_data, label='Траектория (скорость)')
-plt.xlabel('Vx (м/с)')
-plt.ylabel('Vy (м/с)')
-plt.title('Траектория движения (скорости)')
-plt.grid()
+def graf_3D():
+    fig = plt.figure(figsize=(7, 4))
+    ax_3d = fig.add_subplot(projection='3d')
+    ax_3d.scatter(position_x, position_y, position_z)
+    ax_3d.set_xlabel('x')
+    ax_3d.set_ylabel('y')
+    ax_3d.set_zlabel('z')
 
-# 5. Траектория по координатам X-Y
-plt.subplot(3, 2, 5)
-plt.plot(position_x_data, position_y_data, label='Траектория X-Y', color='blue')
-plt.xlabel('X (м)')
-plt.ylabel('Y (м)')
-plt.title('Траектория по X-Y')
-plt.grid()
+# Построение всех графиков сразу при запуске
+plt.figure(figsize=(10, 8))  # Создаем первое окно для графиков
+speed_graf()
+plt.figure(figsize=(10, 8))  # Новое окно для графиков координат
+coords_graf()
+plt.figure(figsize=(10, 8))  # Новое окно для траектории
+traectory_graf()
+plt.figure(figsize=(10, 8))  # Новое окно для графиков высоты
+height_graf()
+plt.figure(figsize=(10, 8))  # Новое окно для 3D графика
+graf_3D()
 
-# 6. Траектория по координатам X-Z
-plt.subplot(3, 2, 6)
-plt.plot(position_x_data, position_z_data, label='Траектория X-Z', color='purple')
-plt.xlabel('X (м)')
-plt.ylabel('Z (м)')
-plt.title('Траектория по X-Z')
-plt.grid()
-
-# Дополнительные графики
-plt.figure(figsize=(12, 8))
-
-# 7. Угол наклона траектории
-trajectory_angle = np.arctan2(velocity_z_data, (velocity_x_data**2 - velocity_y_data**2)**0.5)
-plt.subplot(2, 2, 1)
-plt.plot(time_data, trajectory_angle, color='blue')
-plt.xlabel('Время (s)')
-plt.ylabel('Угол наклона (градусы)')
-plt.title('Угол наклона траектории от времени')
-plt.grid()
-
-# 8. Высота от времени
-plt.subplot(2, 2, 2)
-plt.plot(time_data, altitude_data, color='green')
-plt.xlabel('Время (s)')
-plt.ylabel('Высота (м)')
-plt.title('Высота от времени')
-plt.grid()
-
-# 9. Дальность полета вдоль поверхности
-# horizontal_distance = (position_x_data**2 - position_y_data**2)**0.5
-horizontal_distance = speed_data * (700000 / altitude_data)
-plt.subplot(2, 2, 3)
-plt.plot(time_data, horizontal_distance, color='purple')
-plt.xlabel('Время (s)')
-plt.ylabel('Дальность (м)')
-plt.title('Дальность полета вдоль поверхности')
-plt.grid()
-
-plt.tight_layout()
+# Отображаем все графики
 plt.show()
